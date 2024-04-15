ff5a0d995a4ca98d7bc683d0ba7d51099b48d42e
Gulivero <nikita_slavin00@mail.ru>
2024-04-15 08:31:16 +0300

    added description binding

--- AutoStrongClient/Models/ImageData.cs
+++ AutoStrongClient/Models/ImageData.cs
@@ -1,10 +1,31 @@
-﻿using System.Windows.Media.Imaging;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
 
 namespace AutoStrongClient.Models;
 
-internal class ImageData
+internal class ImageData : INotifyPropertyChanged
 {
     public string Name { get; set; }
-    public string? Description { get; set; }
     public BitmapImage Image { get; set; }
+
+    private string? description;
+    public string? Description
+    {
+        get => description;
+        set
+        {
+            description = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    public void OnPropertyChanged([CallerMemberName] string prop = "")
+    {
+        if (PropertyChanged is not null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
 }
